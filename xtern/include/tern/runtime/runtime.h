510b2e45888c55c33600e37dcf7f9bb194c8097f
wangcheng <WangCheng@wangchengdeMacBook-Air.local>
2016-02-15 00:25:27 +0800

    add Runtime::__pthread_exit

--- xtern/include/tern/runtime/runtime.h
+++ xtern/include/tern/runtime/runtime.h
@@ -7,6 +7,7 @@ struct Runtime {
 
   // thread management
   static int __pthread_create(pthread_t *th, const pthread_attr_t *a, void *(*func)(void*), void *arg);
+  static void __pthread_exit(void *value_ptr);
 };
 
 }
--- xtern/lib/runtime/runtime.cpp
+++ xtern/lib/runtime/runtime.cpp
@@ -8,3 +8,7 @@ int Runtime::__pthread_create(pthread_t *th, const pthread_attr_t *a, void *(*fu
   ret = pthread_create(th, a, func, arg);
   return ret;
 }
+
+void Runtime::__pthread_exit(void *value_ptr) {
+  pthread_exit(value_ptr);
+}
