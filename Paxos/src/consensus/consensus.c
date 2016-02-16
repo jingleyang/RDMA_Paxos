2e76ea726eaf489aed09eb8c874f2946f2c746f5
wangcheng <WangCheng@wangchengdeMacBook-Air.local>
2016-02-16 22:21:35 +0800

    bug fixing

--- Paxos/src/consensus/consensus.c
+++ Paxos/src/consensus/consensus.c
@@ -56,7 +56,7 @@ consensus_component* init_consensus_comp(const char* config_path, const char* lo
     if(NULL != comp){
         comp->node_id = node_id;
         if(*start_mode == 's'){
-            comp->cur_viewview_id = 1;
+            comp->cur_view.view_id = 1;
             comp->cur_view.leader_id = comp->node_id;
             comp->cur_view.req_id = 0;
         }
@@ -67,7 +67,7 @@ consensus_component* init_consensus_comp(const char* config_path, const char* lo
         }else{
             if((build_log_ret = mkdir(log_path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)) != 0){
                 if(errno != EEXIST){
-                    err_log("Log Directory Creation Failed, No Log Will Be Recorded.\n");
+                    con_err_log("Log Directory Creation Failed, No Log Will Be Recorded.\n");
                 }else{
                     build_log_ret = 0;
                 }
@@ -84,7 +84,7 @@ consensus_component* init_consensus_comp(const char* config_path, const char* lo
             }
         }
 
-        if(comp->cur_view->leader_id == comp->node_id){
+        if(comp->cur_view.leader_id == comp->node_id){
             comp->my_role = LEADER;
         }else{
             comp->my_role = SECONDARY;
@@ -133,7 +133,7 @@ int rsm_op(struct consensus_component_t* comp, void* data, size_t data_size){
     view_stamp next = get_next_view_stamp(comp);
 
     /* record the data persistently */
-    db_key_type record_no = vstol(&next);
+    db_key_type record_no = vstol(next);
     request_record* record_data = (request_record*)malloc(data_size + sizeof(request_record));
     gettimeofday(&record_data->created_time, NULL);
     record_data->bit_map = (1<<comp->node_id);
@@ -186,6 +186,15 @@ handle_submit_req_exit:
     return ret;
 }
 
+static void* build_accept_ack(consensus_component* comp, view_stamp* vs){
+    accept_ack* msg = (accept_ack*)malloc(ACCEPT_ACK_SIZE);
+    if(NULL != msg){
+        msg->node_id = comp->node_id;
+        msg->msg_vs = *vs;
+    }
+    return msg;
+};
+
 void handle_accept_req(consensus_component* comp)
 {
     int my_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -198,23 +207,23 @@ void handle_accept_req(consensus_component* comp)
         if (new_entry->req_canbe_exed.view_id != 0)
         {
             CON_LOG(comp, "Node %d Handle Accept Req.\n", comp->node_id);
-            if(new_entry->msg_vs.view_id < comp->cur_view->view_id){
+            if(new_entry->msg_vs.view_id < comp->cur_view.view_id){
                 // TODO
                 //goto reloop;
             }
             // if we this message is not from the current leader
-            if(new_entry->msg_vs.view_id == comp->cur_view->view_id && new_entry->node_id != comp->cur_view->leader_id){
+            if(new_entry->msg_vs.view_id == comp->cur_view.view_id && new_entry->node_id != comp->cur_view.leader_id){
                 // TODO
                 //goto reloop;
             }
 
             // update highest seen request
             if(view_stamp_comp(&new_entry->msg_vs, comp->highest_seen_vs) > 0){
-                *(comp->highest_seen_vs) = new_entry->msg_vs;
+                comp->highest_seen_vs = new_entry->msg_vs;
             }
 
             db_key_type record_no = vstol(&new_entry->msg_vs);
-            request_record* origin_data = (request_record*)msg->data;
+            request_record* origin_data = (request_record*)new_entry->data;
             request_record* record_data = (request_record*)malloc(REQ_RECORD_SIZE(origin_data));
 
             gettimeofday(&record_data->created_time, NULL);
@@ -230,7 +239,7 @@ void handle_accept_req(consensus_component* comp)
             accept_ack* reply = build_accept_ack(comp, &new_entry->msg_vs);
 
             accept_ack* offset = (accept_ack*)(SHM_DATA->shm[new_entry->node_id]);
-            for (int i = 0; i < com->node_id; ++i)
+            for (int i = 0; i < comp->node_id; ++i)
             {
                 offset++;
             }
@@ -248,18 +257,8 @@ void handle_accept_req(consensus_component* comp)
                     retrieve_record(comp->db_ptr, sizeof(index), &index, &data_size, (void**)&record_data);
                     send(my_socket, record_data, data_size, 0);
                 }
-                *(comp->highest_to_commit_vs) = new_entry->req_canbe_exed;
+                comp->committed = new_entry->req_canbe_exed;
             }
         }
     }
-};
-
-
-static void* build_accept_ack(consensus_component* comp, view_stamp* vs){
-    accept_ack* msg = (accept_ack*)malloc(ACCEPT_ACK_SIZE);
-    if(NULL != msg){
-        msg->node_id = comp->node_id;
-        msg->msg_vs = *vs;
-    }
-    return msg;
 };
