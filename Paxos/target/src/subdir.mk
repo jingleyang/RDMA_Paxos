3f4ba4bb04f8950f582dec937aef6d1abd95d8f5
wangcheng <WangCheng@wangchengdeMacBook-Air.local>
2016-02-16 14:55:25 +0800

    try to build

--- Paxos/target/src/subdir.mk
+++ Paxos/target/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/main.c 
+../src/spec_hooks.c 
 
 OBJS += \
-./src/main.o 
+./src/spec_hooks.o 
 
 C_DEPS += \
-./src/main.d 
+./src/spec_hooks.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc-4.8 -std=gnu11 -DDEBUG=$(DEBUGOPT) -I"$(ROOT_DIR)/../.local/include" -O0 -g3 -Wall -pg -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc-4.8 -fPIC -rdynamic -std=gnu11 -DDEBUG=$(DEBUGOPT) -I"$(ROOT_DIR)/../.local/include" -O0 -g3 -Wall -pg -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
