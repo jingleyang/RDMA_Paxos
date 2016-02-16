812bb6dfc791ae22de79d5f5357a64b3c94cbcf1
wangcheng <WangCheng@wangchengdeMacBook-Air.local>
2016-02-16 11:34:50 +0800

    try to build the project

--- Paxos/mk.sh
+++ Paxos/mk.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+#build dep for the program
+# to be added
+# build dep
+# sudo apt-get install libtool
 
 CUR_DIR=$(pwd)
 LIB_PREFIX=${CUR_DIR}/.local/
@@ -7,6 +11,9 @@ mkdir -p ${LIB_PREFIX}
 
 LIBCONFIG_VER=1.4.9
 LIBCONFIG_NAME=libconfig-${LIBCONFIG_VER}
+
+LIBEVENT_VER=2.0.21
+LIBEVENT_NAME=libevent-release-${LIBEVENT_VER}-stable
 
 BDB_VER=5.1.29
 BDB_NAME=db-${BDB_VER}
@@ -30,6 +37,25 @@ pwd
 make;
 make install;
 cd ..
+
+
+if [ ! -f "${LIBEVENT_NAME}.tar.gz" ];then
+    wget --no-check-certificate https://github.com/downloads/libevent/libevent/${LIBEVENT_NAME}.tar.gz
+    wget  https://codeload.github.com/libevent/libevent/tar.gz/release-${LIBEVENT_VER}-stable -O ${LIBEVENT_NAME}.tar.gz
+fi
+
+if [ ! -d "${LIBEVENT_NAME}" ];then
+    tar -xvf ${LIBEVENT_NAME}.tar.gz
+fi
+
+cd ${LIBEVENT_NAME}
+pwd
+./autogen.sh
+./configure --prefix=${LIB_PREFIX}
+make;
+make install;
+cd ..
+
 
 if [ ! -f "${BDB_NAME}.tar.gz" ];then
     wget http://download.oracle.com/berkeley-db/${BDB_NAME}.tar.gz
