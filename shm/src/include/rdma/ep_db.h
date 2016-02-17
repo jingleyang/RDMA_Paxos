4be8ee097c64142de118607f968bd273feca992f
wangcheng <WangCheng@wangchengdeMacBook-Air.local>
2016-02-17 12:49:14 +0800

    exclude RDMA part

--- shm/src/include/rdma/ep_db.h
+++
@@ -1,8 +0,0 @@
-struct ep_t {
-    struct rb_node node;
-    ud_ep_t ud_ep;
-    uint64_t last_req_id;   /* this is the ID of the last request from 
-                            this endpoint that I answer; ignore requests 
-                            with lower IDs */
-};
-typedef struct ep_t ep_t;
--- shm/src/include/rdma/servers.h
+++
@@ -1,4 +0,0 @@
-struct servers_t {
-    void *ep;               // endpoint data (network related)
-};
-typedef struct servers_t servers_t;
